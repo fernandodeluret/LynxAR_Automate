ae5de619f7add83f6e97db41b37e1c8dea9c54bf
fernando deluret <fernandodeluret@gmail.com>
2020-06-19 03:33:58 -0300

    prototipo inicial solo con conexion local

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+
 
 export default function App() {
+
+  const [ligth, setLigth] = useState(false);
+  let lightState
+
+  async function switchLight(v) {
+    try {
+      let response = await fetch('http://192.168.1.200/interruptor');
+      let responseJson = await response.json();
+      console.log(responseJson);
+    } catch (error) {
+      console.error(error);
+    }    
+  }
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>LynxAR</Text>
+      <Switch
+        value={ligth}
+        onValueChange={v => {
+          setLigth(v)
+          switchLight(v)
+        }}
+      />
+      <Text>{ligth? 'On': 'Off'}</Text>
+      
+      
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
